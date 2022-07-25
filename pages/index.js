292419ad23dc85542b650d2a31372b0b66143a17
jamescardona11 <jjcardonao@gmail.com>
2022-07-24 21:45:01 -0500

    add contact and team components

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ import {
   Networks,
   TopCreator,
 } from '@/components'
+import { Contacts } from '@/components/Contacts'
 
 import React from 'react'
 
@@ -31,17 +32,8 @@ const Home = () => {
         <Pricing />
         <AboutUs />
         <FAQ />
-        <Container>
-          <div
-            className={
-              'bg-gradient-to-b from-[#B75CFF] to-[#671AE4] rounded-xl py-20 px-10 flex items-center justify-center shadow-lg flex-col mb-44'
-            }
-          >
-            <h2 className="font-bold text-2xl sm:text-4xl lg:text-5xl mb-5 w-full md:w-2/5 leading-snug text-center">
-              Get ready to collect our NFT
-            </h2>
-          </div>
-        </Container>
+
+        <Contacts />
         <Footer />
       </div>
       <div className="banner"></div>
